4acaf175092e0810e947e34c53ec3b6bdd2a2863
이지은 <jieun7056@naver.com>
2026-09-17 12:03:51 +0900

    기본 세팅

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -2,7 +2,9 @@ package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // jpa에서 엔티티 생성일자, 수정일자를 자동으로 관리
 @SpringBootApplication
 public class BackApplication {
 
