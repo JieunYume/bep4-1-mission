451ab994e5eebed6181f429a1aa1a41cce5950db
이지은 <jieun7056@naver.com>
2026-09-17 12:22:52 +0900

    글 6개 생성

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // jpa에서 엔티티 생성일자, 수정일자를 자동으로 관리
+@EnableJpaAuditing // jpa에서 엔티티의 생성일자, 수정일자를 자동으로 관리
 @SpringBootApplication
 public class BackApplication {
 
