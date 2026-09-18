596d69fe6af8ad15bc8f3c4c16a1d1ffad892851
이지은 <jieun7056@naver.com>
2026-09-18 12:25:00 +0900

    feat: Member의 공통 필드를 묶은 BaseMember 도입

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,7 +1,6 @@
 package com.back.boundedContext.post.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTimeManual;
-import jakarta.persistence.Column;
+import com.back.shared.member.domain.ReplicaMember;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -10,11 +9,9 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "POST_MEMBER")
 @NoArgsConstructor
-@Getter
-public class PostMember extends BaseIdAndTimeManual {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+@Getter // STUDY: 얘는 왜 Getter를 갖지? BaseMember가 갖고있는데 다른건가?
+public class PostMember extends ReplicaMember {
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
 }
