6e09e87e1952f3b8f59631152a930c6917c57428
이지은 <jieun7056@naver.com>
2026-09-18 10:23:59 +0900

    feat: 글 작성시 회원에 관련된 랜덤 팁 제공(일부러 결합을 만듬)

--- src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
+++ src/main/java/com/back/boundedContext/member/app/MemberJoinUseCase.java
@@ -19,6 +19,8 @@ public class MemberJoinUseCase {
 
         Member member = memberRepository.save(new Member(username, password, nickname));
 
+
         return new RsData<>("201-1", "%d번 회원이 생성되었습니다.".formatted(member.getId()), member);
+
     }
 }
