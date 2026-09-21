0b6e5b55885f3772211c78d2ffa8f78ff22f58f4
이지은 <jieun7056@naver.com>
2026-09-21 10:44:40 +0900

    refactor: PostFacade를 유즈케이스로 분리하여 repository 의존성 제거

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -16,39 +16,32 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
     private final PostWriteUseCase postWriteUseCase;
-    private final PostMemberRepository postMemberRepository;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
+    private final PostSupport postSupport;
 
-    public long count() {
-        return postRepository.count();
+    @Transactional
+    public PostMember syncMember(MemberDto member) {
+        return postSyncMemberUseCase.syncMember(member);
     }
 
+    @Transactional
     public RsData<Post> write(PostMember author, String title, String content) {
         return postWriteUseCase.write(author, title, content);
     }
 
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+    @Transactional(readOnly = true)
+    public long count() {
+        return postSupport.count();
     }
 
-    @Transactional
-    public PostMember syncMember(MemberDto member) {
-        PostMember _member = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
-
-        return postMemberRepository.save(_member);
+    @Transactional(readOnly = true)
+    public Optional<Post> findById(int id) {
+        return postSupport.findById(id);
     }
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username){
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findMemberByUsername(username);
     }
 }
