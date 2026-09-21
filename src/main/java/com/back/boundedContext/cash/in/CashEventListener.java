254b58469bd1f53f914ac0c4ba574dcad77f4d0d
이지은 <jieun7056@naver.com>
2026-09-21 17:45:44 +0900

    feat: 주문 결제처리

--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.cash.in;
 
 import com.back.boundedContext.cash.app.CashFacade;
 import com.back.shared.cash.event.CashMemberCreatedEvent;
+import com.back.shared.market.event.MarketOrderPaymentRequestedEvent;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -33,5 +34,11 @@ public class CashEventListener {
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(CashMemberCreatedEvent event) { // STUDY: 이건 무조건 cashFacade의 syncMember에서 호출해야 하는데 따로 그걸로만 제한할 수 있는 방법이 있을까?
         cashFacade.createWallet(event.getMember());
+    }
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MarketOrderPaymentRequestedEvent event) {
+        cashFacade.handle(event);
     }
 }
