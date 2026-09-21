2fcc99627d60595790dfd14dbf8da16c7b0fb88d
이지은 <jieun7056@naver.com>
2026-09-21 10:32:57 +0900

    refactor: MemberJoinedEvent에 있는 비즈니스 로직(createWallet)을 새로 이벤트를 발행하여 제거

--- src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
@@ -2,17 +2,21 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
+import com.back.boundedContext.cash.out.CashMemberRepository;
 import com.back.boundedContext.cash.out.WalletRepository;
+import com.back.shared.cash.dto.CashMemberDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class CashCreateWalletUseCase {
+    private final CashMemberRepository cashMemberRepository;
     private final WalletRepository walletRepository;
 
-    public Wallet createWallet(CashMember holder){
-        Wallet wallet = new Wallet(holder);
+    public Wallet createWallet(CashMemberDto member){
+        CashMember _member = cashMemberRepository.getReferenceById(member.getId()); // NOTE: findById보다 좀 더 빨리 탐색한다.
+        Wallet wallet = new Wallet(_member);
 
         return walletRepository.save(wallet);
     }
--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -1,7 +1,7 @@
 package com.back.boundedContext.cash.in;
 
 import com.back.boundedContext.cash.app.CashFacade;
-import com.back.boundedContext.cash.domain.CashMember;
+import com.back.shared.cash.event.CashMemberCreatedEvent;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -20,14 +20,18 @@ public class CashEventListener {
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberJoinedEvent event) {
-        CashMember member = cashFacade.syncMember(event.getMember());
-
-        cashFacade.createWallet(member);
+        cashFacade.syncMember(event.getMember());
     }
 
     @TransactionalEventListener(phase = AFTER_COMMIT)
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberModifiedEvent event) {
         cashFacade.syncMember(event.getMember());
+    }
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(CashMemberCreatedEvent event) { // STUDY: 이건 무조건 cashFacade의 syncMember에서 호출해야 하는데 따로 그걸로만 제한할 수 있는 방법이 있을까?
+        cashFacade.createWallet(event.getMember());
     }
 }
