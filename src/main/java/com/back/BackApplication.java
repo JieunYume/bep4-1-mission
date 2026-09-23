3ce6f95136a41b4cd0f173944f37acca76ee8fff
이지은 <jieun7056@naver.com>
2026-09-23 15:16:06 +0900

    feat: 배치 잡을 스케줄러로 1, 4, 22시에 실행되도록 설정

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -3,13 +3,13 @@ package com.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing // jpa에서 엔티티의 생성일자, 수정일자를 자동으로 관리
 @SpringBootApplication
+@EnableScheduling
 public class BackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
     }
-
 }
